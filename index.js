07ec8c080a8bee9c0f0168793d943865a028d993
Fabian Rosinger <fabianrosinger@gmail.com>
2019-11-25 21:12:27 +0100

    Add ignore gitignore, add eslint-config-standard

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-/* Server  */
-import express from 'express';
-import mongoose from 'mongoose';
+/* Server */
+import express from 'express'
+import mongoose from 'mongoose'
 
 // Needed for reading from .env
-require('dotenv').config();
+require('dotenv').config()
 
-
-const app = express();
-
+const app = express()
 // Connect to MongoDB
 /* mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true }); // Connect to database
-const db = mongoose.connection;
+const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('connected to database')) */
 
 // Define what app needs to use
-app.use(express.json()); // Accept json
+app.use(express.json()) // Accept json
 
-app.listen(3000, () => console.log('server started'));
+app.listen(3000, () => console.log('server started'))
