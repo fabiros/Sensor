53aa5c3e4c18e2941f5948eb8d758b66fb2bf95e
Fabian Rosinger <fabianrosinger@gmail.com>
2019-11-25 22:31:52 +0100

    Add findByName method to UserSchema

--- models/user.js
+++ models/user.js
@@ -1,16 +1,20 @@
-import {
-    Schema,
-    model
-} from 'mongoose'
-
+import { Schema,model } from 'mongoose'
 
 const UserSchema = new Schema({
     firstName: String,
     lastName: String,
-    fullName: String,
+    displayName: String,
     username: String,
     createdBy: String,
     createDate: Date,
+})
+
+UserSchema.method('findByName', (name, cb) => {
+    return this.findOne({
+        username: name
+    }, (err, user) => {
+        return cb(err, user)
+    })
 })
 
 export default model('User', UserSchema)
