2588ab8661faacff8333a30c52bdf598e54a192e
Fabian Rosinger <fabianrosinger@gmail.com>
2019-11-26 22:29:33 +0100

    Fix prettier and eslint, remove linting errors, add functionality to reigster

--- routes/index.js
+++ routes/index.js
@@ -3,37 +3,47 @@ import User from '../models';
 
 const router = Router();
 
-/* const register = (req, res, next) => {
+const register = (req, res, next) => {
     if (!req.params.username) {
-    return next(new Error('Username must be set.'));
-  }
-  if (!req.params.password) return next(new Error('Password must be set.'))
-  if (!req.params.email) return next(new Error('Email must be set.'))
+        return next(new Error('Username must be set.'));
+    }
+    if (!req.params.password) {
+        return next(new Error('Password must be set.'));
+    }
+    if (!req.params.email) {
+        return next(new Error('Email must be set.'));
+    }
 
-  User.findByName(req.params.username, (err, user) => {
-    if (err) return next(err)
-
-    // if user not found register
-    if (!user) {
-      User.register(req.body.username, req.body.password, req.body.email, req.body.ldap, function (err, user) {
-        user.createDate = new Date()
-        // send if triggered but not for ldap users
-        if (req.body.send && !user.ldap) {
-          // send credentials email if requested
-          agenda.now('sendCredentials', {
-            user: user.username,
-            email: user.email,
-            password: req.body.password,
-            baseUri: req.getBaseUrl()
-          })
+    User.findByName(req.params.username, (error, user) => {
+        if (error) {
+            return next(error);
         }
-        res.json({
-          status: 'ok'
-        })
-      })
-  } else return next(new Error('User already registered'))
-})
-} */
+
+        // if user not found register
+        if (!user) {
+            User.register(
+                req.body.username,
+                req.body.password,
+                req.body.email,
+                (err, newUser) => {
+                    if (err || !newUser) {
+                        return next(
+                            new Error(
+                                'Something happend. Could not create new User'
+                            )
+                        );
+                    }
+                    res.json({
+                        status: 'ok',
+                    });
+                    return next();
+                }
+            );
+        } else {
+            return next(new Error('User already registered'));
+        }
+    });
+};
 
 // Routes
 router.get('/', (req, res) => {
